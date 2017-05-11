5d8cb273803f7bc71cd83d3273bf582595574017
Py-get <jack.harwood.ellenbrook@gmail.com>
2017-05-11 19:37:09 +0800

    Random is not needed.
    Was leftover from prior project.

--- 4chScrape.py
+++ 4chScrape.py
@@ -2,7 +2,6 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import urllib.request
 import re
-import random
 import os
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
